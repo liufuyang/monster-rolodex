c14a5d0d994964e674bde7fece8e65549f9f4b3c
Fuyang Liu <liufuyang@users.noreply.github.com>
2022-10-10 08:34:48 +0200

    feat: add class state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // local state
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'Fuyang'
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,14 +21,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name}</p>
+          <button>Change Name</button>
         </header>
         <div>
           G1
